46f0bed588d9bff89feef6bcfec2049c8faba321
ApricotLace <profunctorlense@gmail.com>
2022-05-30 12:39:29 +0300

    Delete useless path exporter
    Co-authored-by: @mput <putintsev@gmail.com>

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,9 +1,5 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Path hack for go local binaries FIXME
-(setenv "PATH" (concat (getenv "PATH")  ":/Users/ghrp/go/bin"))
-
-
 (setq user-full-name "Evgeny Mukha"
       user-mail-address "Profunctorlense@gmail.com")
 
