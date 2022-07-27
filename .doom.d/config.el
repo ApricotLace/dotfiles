023522821868d1c518bf57f9416e6571db3fb366
Evgeny Mukha <profunctorlense@gmail.com>
2022-07-27 16:05:21 +0300

    lsp stuff

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -100,6 +100,8 @@
         "c" #'sql-connect))
       (:leader
        (:map (clojure-mode-map clojurescript-mode-map emacs-lisp-mode-map)
+        (:prefix ("l" . "lsp")
+         "r" #'lsp-ui-peek-find-references)
         (:prefix ("m" . "cider")
          "e ;" #'cider-pprint-eval-last-sexp)
         (:prefix ("k" . "lisp")
